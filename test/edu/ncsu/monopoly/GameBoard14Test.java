acae9ae74f882f6fb68557dfbfda817b93f29b7f
cmassie <massie@Christophers-MacBook-Air.local>
2016-10-25 11:25:58 -0600

    initial commit

---
+++ test/edu/ncsu/monopoly/GameBoard14Test.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ncsu.monopoly;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author massie
+ */
+public class GameBoard14Test {
+    
+    public GameBoard14Test() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
---
+++ test/edu/ncsu/monopoly/PlayerTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ncsu.monopoly;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author massie
+ */
+public class PlayerTest {
+    
+    public PlayerTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
