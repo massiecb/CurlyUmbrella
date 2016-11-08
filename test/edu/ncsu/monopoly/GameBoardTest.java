7a41df12aa70e7d0a8f78dd6c80bdf268f5c7ada
cmassie <massie@Christophers-MacBook-Air.local>
2016-11-08 00:27:06 -0700

    added test for game board drawn cards to enable reshuffle

--- test/edu/ncsu/monopoly/GameBoardTest.java
+++ test/edu/ncsu/monopoly/GameBoardTest.java
@@ -13,12 +13,30 @@ import static org.junit.Assert.*;
  * @author massie
  */
 public class GameBoardTest {
+    GameBoard GB;
     
     public GameBoardTest() {
     }
 
     @Test
-    public void testSomeMethod() {
+    public void testCCLastCardDrawn() {
+        GB = new GameBoard();
+        MoneyCard cardOne = new MoneyCard("Win $50", 50, Card.TYPE_CC);
+        GB.addCard(cardOne);
+        GB.drawCCCard();
+        assertEquals(GB.drawCCCard().getLabel(), cardOne.getLabel());
+    }
+    
+    @Test
+    public void testCCMoreThanOneCard(){
+        GB = new GameBoard();
+        MoneyCard cardOne = new MoneyCard("Win $50", 50, Card.TYPE_CC);
+        MoneyCard cardTwo = new MoneyCard("Win $51", 51, Card.TYPE_CC);
+        GB.addCard(cardOne);
+        GB.addCard(cardTwo);
+        assertEquals(GB.drawCCCard().getLabel(), cardOne.getLabel());
+        assertEquals(GB.drawCCCard().getLabel(), cardTwo.getLabel());
+        
     }
     
 }
