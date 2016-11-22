5f743d562bd7bc671774e1c65698d75048d75aef
cmassie <massie@Christophers-MacBook-Air.local>
2016-11-21 23:56:29 -0700

    some clean up. left some testing in by mistake

--- src/edu/ncsu/monopoly/gui/TempMVCTest.java
+++ src/edu/ncsu/monopoly/gui/TempMVCTest.java
@@ -4,49 +4,31 @@ import edu.ncsu.monopoly.Player;
 import edu.ncsu.monopoly.*;
 import java.util.ArrayList;
 import java.util.List;
+import static javafx.application.Platform.exit;
 /**
  *
  * @author massie
  */
 public class TempMVCTest {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        /*
-        // TODO code application logic here
         GameMaster master = GameMaster.instance();
-        MainWindow w = new MainWindow();
-        GameBoard gb = new GameBoardFull();
-        master.setGameBoard(gb);
-        GameMaster.instance().setNumberOfPlayers(1);
-        w.setupGameBoard(gb); 
-       master.setGUI(w);
-        master.startGame();        */
-        GameMaster master = GameMaster.instance();
- 
-		master = GameMaster.instance();
-		master.setGameBoard(new SimpleGameBoard());
+        master = GameMaster.instance();
+        master.setGameBoard(new SimpleGameBoard());
         master.setGUI(new MockGUI());
         master.setTestMode(true);
         master.reset();
         master.setNumberOfPlayers(1);
-        System.out.println(master.getPlayer(0).getPosition());
         master.movePlayer(0, 1);
         Player p = master.getPlayer(0);
         p.purchase();
         master.movePlayer(0, 1);
-        System.out.println(p.getPosition());
-        p.purchase();
-        master.movePlayer(0, 1);
-        System.out.println(p.getPosition());
         p.purchase();
         master.movePlayer(0, 1);
         p.purchase();
-        System.out.println(p.getPosition());
         master.movePlayer(0, 1);
-        System.out.println(p.getPosition());
+        p.purchase();
+        master.movePlayer(0, 1);
         p.purchase();
 
         BuyHouseView view = new BuyHouseView();
