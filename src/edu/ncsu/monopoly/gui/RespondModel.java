455592d92828ee3a83b311f9c823223b29e027f9
cmassie <massie@Christophers-MacBook-Air.local>
2016-11-24 13:43:18 -0700

    started refactoring respond

---
+++ src/edu/ncsu/monopoly/gui/RespondModel.java
@@ -0,0 +1,43 @@
+package edu.ncsu.monopoly.gui;
+
+import edu.ncsu.monopoly.RespondDialog;
+import edu.ncsu.monopoly.TradeDeal;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author massie
+ */
+public class RespondModel implements RespondDialog{
+    
+    private boolean response;
+    private RespondView view;
+    
+    RespondModel(RespondView view, TradeDeal deal){
+        this.view = view;
+        view.addAcceptButtonListener(new AcceptButtonAction());
+        view.addDeclineButtonListener(new DeclineButtonAction());
+        view.setMessageText(deal.makeMessage());
+    }
+    
+    public class AcceptButtonAction implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e){
+            response = true;
+        }
+    }
+    
+    public class DeclineButtonAction implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e){
+            response = false;
+        }
+    }
+    
+    @Override
+    public boolean getResponse() {
+        return response;
+    }
+    
+}
